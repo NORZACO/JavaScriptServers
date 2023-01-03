1de3a9949b2b826a68a29d2d464d17c91da602db
Mwamuzi Shadrick <norzacoas@gmail.com>
2023-01-03 12:25:51 +0100

    proccess argument

--- Templates/static/globalAcess.js
+++ Templates/static/globalAcess.js
@@ -1,30 +1,46 @@
-let processid = process.pid
-let processVersion = process.versions.node
-let processPlatform = process.platform
-let processArch = process.arch
-let processTitle = process.title
-let processArgv = process.argv
-let processEnv = process.env
+const { 
+    pid: processid, versions: { node: processVersion }, 
+    platform: processPlatform, arch: processArch, title: 
+    processTitle, argv: processArgv, env: processEnv } = process;
 
-console.log(processid)
-console.log(processVersion)
-console.log(processPlatform)
-console.log(processArch)
-console.log(processTitle)
+console.log(processid);
+console.log(processVersion);
+console.log(processPlatform);
+console.log(processArch);
+console.log(processTitle);
 console.log(processArgv);
-console.log(processEnv['IP_ADDRESS']);
+// console.log(processEnv);
 
 
+console.log("DJANGO_SECRET_KEY_TWO :" + process.env['DJANGO_SECRET_KEY_TWO']);
 
-const grab = flag => {
-    let indexAfterFlag = process.argv.indexOf(flag) + 1;
-    return process.argv[indexAfterFlag];
+
+// grap flag function
+const grap = flag => {
+    let index = process.argv(flag);
+    return 
 }
 
 
-const [,, firstName, lastName ] = processArgv
 
-const greeting = grab("--greeting");
-const user = grab("--user");
+let greeting = grab("--user");
+let username = grab("--greeting")
 
-console.log(`Your name is ${greeting} ${user}`);
+
+
+const [, , firstName, lastName] = process.argv
+let user = `My name is ${firstName} ${lastName}`
+console.log(user);
+
+process.stdout.write("What's your name?!\n");
+process.stdin.on("data", name => {
+    process.stdout.write(`Hello ${name.toString().trim()}!`);
+    process.exit();
+})
+
+// process.stdout.write("What do you what to do?!\n");
+// process.stdin.on("data", username => {
+//     process.stdout.write(`Login ${`${username.toString().trim()}!` }`);
+//     //process.stdout.write("Hello " + name.toString() + "!\n");
+//     process.exit();
+// })
